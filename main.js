093e0ca85eba304740885028768329e643e496a0
jakejamesreid <jakejamesreid@gmail.com>
2020-03-26 16:54:47 +0000

    Print result to console using a callback instead of a timeout

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-var xhr = new XMLHttpRequest();
+function getData(cb){
+    // xhr = XmlHttpRequest
+    var xhr = new XMLHttpRequest(); 
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+    xhr.open("GET", "https://swapi.co/api/");
+    xhr.send();
 
-xhr.open("GET", "https://swapi.co/api/");
+    /* xhr Ready States
+    0 Unsent
+    1 Opened
+    2 Headers Received
+    3 Loading
+    4 Done
+    */
+    xhr.onreadystatechange = function() {
 
-xhr.send();
+        // When Data is received
+        if (this.readyState == 4 && this.status == 200){
+            // Change the inner HTML of the DIV to JSON response text
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data){
+    console.log(data);
+}
+
+// Use Inspect in Google Chrome to view result
+getData(printDataToConsole);
